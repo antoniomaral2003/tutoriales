5fb877e480c2de7e49360dbb8ee26dbdfcb9821c
antoniomartin <amarala275@g.educaand.es>
2023-02-14 12:53:07 +0100

    Tareas JPA

--- src/main/java/org/iesvdm/tutoriales/domain/Categoria.java
+++ src/main/java/org/iesvdm/tutoriales/domain/Categoria.java
@@ -1,4 +1,27 @@
 package org.iesvdm.tutoriales.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+
+
+
 }
